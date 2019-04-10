d81f5160935e0959b81ede417525ec7b3c912663
Nishith P <nishithp11@gmail.com>
2019-04-10 18:32:48 +0530

    Create type.js

---
+++ type.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded',function(event){
+  var dataText = [ "Hi,I'm Nishith P ", "Coder", " Ethical Hacker", "Google Code-in contestant", "Open Source contributor", "Google enthusiast", "Developer", "14"];
+  function typeWriter(text, i, fnCallback) {
+    if (i < (text.length)) {
+     document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+    else if (typeof fnCallback == 'function') {
+      setTimeout(fnCallback, 700);
+    }
+  }
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 20000);
+     }
+    if (i < dataText[i].length) {
+     typeWriter(dataText[i], 0, function(){
+       StartTextAnimation(i + 1);
+     });
+    }
+  }
+  StartTextAnimation(0);
+});
